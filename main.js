a412530d006ca69349f2ac8a6d6022b071121374
D3vls <dhruv.sahani.1807@gmail.com>
2025-08-28 16:08:42 +0530

    merge final base project setup with main.js and index.html

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log("Hello, Desktop Application!");
+//we are importing two Electron modules with CommonJS module syntax:
+
+// app, which controls your application's event lifecycle.
+// BrowserWindow, which creates and manages app windows.
+import { app, BrowserWindow } from "electron/main";
+
+// The createWindow() function loads your web page into a new BrowserWindow instance
+// and sets the window's dimensions to 800x600 pixels.
+// The loadFile("index.html") method loads the specified HTML file into the window.
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+
+// The app.whenReady() method ensures that the createWindow() function is called only after Electron has finished initializing.
+// This prevents any issues that might arise from trying to create a window before the app is fully
+app.whenReady().then(() => {
+  createWindow();
+
+  // The app.on("activate") event listener is specific to macOS (darwin).
+  // It ensures that if the application is activated (e.g., clicked in the dock) and there are no open windows,
+  // a new window is created. This is a common behavior on macOS where applications often
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// The window-all-closed event is emitted when all windows have been closed.
+// The code checks if the platform is not macOS (darwin) and calls app.quit
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
